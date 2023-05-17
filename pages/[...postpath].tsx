174420eb906aa9e5513dc6e7acb2191b4428ab6c
vireakp <57161784+vireakp@users.noreply.github.com>
2023-05-17 22:26:23 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://seameoted.org/newsfeed` + encodeURI(path as string)
+					`https://seameoted.org/newsfeed/` + encodeURI(path as string)
 				}`,
 			},
 		};
